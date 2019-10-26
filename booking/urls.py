67f74cbd81eaba84cdba5d88b9e5803e4af57cc6
Nour El-Sharkawi <Nour@Nours-MacBook-Pro-703.local>
2019-10-26 14:31:54 +0300

    library updated

--- booking/urls.py
+++ booking/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from rest_framework_jwt.views import obtain_jwt_token
-
+# from rest_framework_jwt.views import obtain_jwt_token
+from rest_framework_simplejwt.views import TokenObtainPairView
 from hotels import views
 
 urlpatterns = [
@@ -33,6 +33,6 @@ urlpatterns = [
 
     path('profile/', views.Profile.as_view(), name="profile"),
 
-    path('login/', obtain_jwt_token, name="login"),
+    path('login/', TokenObtainPairView.as_view(), name="login"),
     path('register/',  views.Register.as_view() , name="register"),
 ]
